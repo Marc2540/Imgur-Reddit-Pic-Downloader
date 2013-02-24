4a2d3aafd94ad7b3855770b01c9647016beadf3b
Marcus <MWQ@live.dk>
2013-02-24 17:53:09 +0100

    Updated syntax to be more pythonic.
    print ('\n rest of the print message') instead of print ('')
    and 'if debug:' instead of 'if debug == True:'

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python 
 #Created in Python 3.3.0
 #Created by Marc2540
-#Version 0.9
+#Version 0.9.1
 
 from time import time, sleep
 from math import ceil
@@ -16,9 +16,6 @@ urllib.request lets me pull things from websites.
 shutil lets me download the results from urlopen.
 the json library lets me decode .json.
 """
-
-
-
 
 
 #settings
@@ -38,20 +35,23 @@ subredditUrl = 'empty'
 finalUrl = 'empty'
 debugModifier = 'none'
 
-if runWithoutConfigFile == True:
+
+if debug:
+    verbose = True
+if runWithoutConfigFile:
     skipFrequencyCheck = True
     skipWriteToConfig = True
 else:
     if len(configFileName) < 2 or len(configFileName) > 100:
         configFileName = 'config.txt'
-        if verbose == True:
+        if verbose:
             print ('Invalid config file name, reverted to "config.txt".')
     #checks if config.txt exists. Creates it if it doesn't exist.
     try:
         with open(configFileName) as f:
             pass
     except:
-        if verbose == True:
+        if verbose:
             print ('Config file doesn\'t exist. Creating it.')
         f = open(configFileName,'w')
         f.write('lastRunTime=0')
@@ -62,9 +62,8 @@ else:
     configFile_Read = open(configFileName, "r")
     for lastRunTime in configFile_Read:
         if "lastRunTime=" in lastRunTime:
-            if debug == True: #debug check
-                print ('')
-                print ('Last runtime was ' + lastRunTime[12:])
+            if debug: #debug check
+                print ('\nLast runtime was ' + lastRunTime[12:])
                 sleep(5)
     configFile_Read.close()
 
@@ -77,7 +76,7 @@ def sinceLastRun():
     """
     if time() >= (int(lastRunTime[12:]) + frequencyCheck):
         askUrl()
-    elif verbose == 'no':
+    elif verbose == False:
         askUrl()
     else:
         print ('According to config.txt, it hasn\'t been ' + str(frequencyCheck) + ' seconds since last run')
@@ -100,9 +99,8 @@ def write_time_to_config_file():
         configFile_Write = open(configFileName, "w")
         configFile_Write.write("lastRunTime=" + str(ceil(time())))
         configFile_Write.close()
-        if debug == True: #debug check
-            print ('')
-            print ('Wrote lastRunTime=' + str(ceil(time())) + ' to config file.')
+        if debug: #debug check
+            print ('\nWrote lastRunTime=' + str(ceil(time())) + ' to config file.')
             sleep(5)
 
     
@@ -119,7 +117,7 @@ def askUrl():
     else:
         askForSubreddit = input('Which subreddit do you want to pull from? (e.g. r/pics) ')
         if str(askForSubreddit) == '':
-            if verbose == True:
+            if verbose:
                 print ('Please try again.')
             askUrl()
         elif askForSubreddit.startswith('/') and askForSubreddit.endswith('/'):
@@ -135,7 +133,7 @@ def askUrl():
             subredditUrl = 'http://reddit.com/' + askForSubreddit + '/.json'
             modifyUrl()
         else:
-            if verbose == True:
+            if verbose:
                 print ('I didn\'t understand you. Are you sure you\'re writing it correctly? (e.g r/pics, /r/pics or /r/pics/)')
             askUrl()
 
@@ -152,7 +150,7 @@ def modifyUrl():
         debugModifier = 'frontpage'
         fetchImg()
     elif str(askForModifiers) != 'modify':
-        if verbose == True:
+        if verbose:
             print ('Please try again.')
         askUrl()
     else:
@@ -206,28 +204,25 @@ def debugPreFetch():
     dont want to repeat the 'if debug == true' statement 1000 times.
     """
     #debugging for askUrl()
-    print ('')
-    print ('The subreddit url to get .json from is ' + subredditUrl)
+    print ('\nThe subreddit url to get .json from is ' + subredditUrl)
     #debugging for modifyUrl()
-    print ('')
-    print ('The chosen modifier is ' + str(debugModifier))
-    print ('The final url to get .json from is ' + str(finalUrl))
+    print ('\nThe chosen modifier is ' + str(debugModifier))
+    print ('\nThe final url to get .json from is ' + str(finalUrl))
     sleep(5)
 
 
 
 def fetchImg():
-    if debug == True:
+    if debug:
         debugPreFetch()
+    
     response = urlopen(finalUrl)
     content = response.read()
     data = json.loads(content.decode("utf8"))
     
-    if debug == True: #debug check
-        print ('')
-        print ('The data from ' + finalUrl + ' is returning this data:')
+    if debug: #debug check
+        print ('\nThe data from ' + finalUrl + ' is returning this data: \n')
         sleep(1)
-        print ('')
         print (data)
         sleep(5)
     
@@ -239,38 +234,34 @@ def fetchImg():
             fileType = img['url'].lower()[-3:]
             domain = img['url'].lower().split("/")[2]
             allowedType = fileType == 'png' or fileType == 'jpg'
-            if debug == True: #debug check
-                print ('')
-                print ('This loop runs if i < ' + str(numberOfLoops) + '. I is currently ' + str(i))
+            if debug: #debug check
+                print ('\nThis loop runs if i < ' + str(numberOfLoops) + '. I is currently ' + str(i))
                 sleep(1)
-                print ('')
-                print ('The current value of "img" is:')
+                print ('\nThe current value of "img" is: \n')
                 sleep(0.5)
-                print ('')
                 print (img)
-                print ('')
                 sleep(2)
-                print ('Is this a self-post? ' + str(img['is_self']))
+                print ('\nIs this a self-post? ' + str(img['is_self']))
                 print ('The go-to url of this post is: ' + img['url'])
                 print ('The domain is ' + domain)
                 print ('What are the 3 last letters of the url? (filetype if valid image) .' + fileType)
                 print ('Is the filetype allowed? ' + str(allowedType))
-                sleep(7.5)
+                sleep(5)
                 
-            if img['is_self'] == True:
-                if verbose == True:
+            if img['is_self']:
+                if verbose:
                     print ('Image is a self-post. Skipped')
             elif allowedType == False:
-                if verbose == True:
+                if verbose:
                     print ('Image isn\'t a png or jpg. Skipped')
             elif domain != 'i.imgur.com':
-                if verbose == True:
+                if verbose:
                     print ('Image isn\'t hosted on an allowed domain, skipping to avoid 404 errors.')
             else:
                 p = img['url'].split("/")[-1]
                 with urlopen(img['url']) as in_stream, open(p, 'wb') as out_file:
                     copyfileobj(in_stream, out_file)
-                    if verbose == True:
+                    if verbose:
                         print ('Saved new image as ' + p)
         except IndexError:
             print ('No data left in .json file.')
@@ -281,6 +272,6 @@ def fetchImg():
         
 
 #actually runs the main part of the script.
-if skipFrequencyCheck == True:
+if skipFrequencyCheck:
     askUrl()
 else: sinceLastRun()
